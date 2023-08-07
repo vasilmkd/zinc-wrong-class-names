3deee227d48c005742b557a728268a5cb5a49b9b
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-08-07 17:34:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Minimal reproduction project which demonstrates that the Zinc incremental compiler reports incorrect classfile names for
+the class files generated during compilation by.
---
+++ src/main/scala/main.scala
@@ -0,0 +1,41 @@
+object OuterLevelWithVeryVeryVeryLongClassName1 {
+  object OuterLevelWithVeryVeryVeryLongClassName2 {
+    object OuterLevelWithVeryVeryVeryLongClassName3 {
+      object OuterLevelWithVeryVeryVeryLongClassName4 {
+        object OuterLevelWithVeryVeryVeryLongClassName5 {
+          object OuterLevelWithVeryVeryVeryLongClassName6 {
+            object OuterLevelWithVeryVeryVeryLongClassName7 {
+              object OuterLevelWithVeryVeryVeryLongClassName8 {
+                object OuterLevelWithVeryVeryVeryLongClassName9 {
+                  object OuterLevelWithVeryVeryVeryLongClassName10 {
+                    object OuterLevelWithVeryVeryVeryLongClassName11 {
+                      object OuterLevelWithVeryVeryVeryLongClassName12 {
+                        object OuterLevelWithVeryVeryVeryLongClassName13 {
+                          object OuterLevelWithVeryVeryVeryLongClassName14 {
+                            object OuterLevelWithVeryVeryVeryLongClassName15 {
+                              object OuterLevelWithVeryVeryVeryLongClassName16 {
+                                object OuterLevelWithVeryVeryVeryLongClassName17 {
+                                  object OuterLevelWithVeryVeryVeryLongClassName18 {
+                                    object OuterLevelWithVeryVeryVeryLongClassName19 {
+                                      object OuterLevelWithVeryVeryVeryLongClassName20 {
+                                        case class MalformedNameExample(x: Int)
+                                      }
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
